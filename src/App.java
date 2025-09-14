ecd6cae00fb96ac0ec578f24ae2054741b531009
Hernande Monteiro <monteiro1998@live.com>
2025-09-14 19:06:14 -0300

    init: first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+
+        int anoDeLancamento = 2022;
+        String nomeDoFilme = "Top Gun: Maverick";
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        System.out.println("Filme: " + nomeDoFilme);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Incluído no Plano: " + (incluidoNoPlano ? "Sim" : "Não"));
+        System.out.println("Nota do Filme: " + notaDoFilme);
+    }
+}
